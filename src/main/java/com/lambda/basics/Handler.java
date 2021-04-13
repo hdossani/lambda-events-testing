00aa4e2741198a70d3a3cd976a4fa74a51fa4d68
hdossani <hiren.dossani@gmail.com>
2021-04-13 16:59:03 -0400

    update for event

--- src/main/java/com/lambda/basics/Handler.java
+++ src/main/java/com/lambda/basics/Handler.java
@@ -2,14 +2,15 @@ package com.lambda.basics;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
-
+@Slf4j
 public class Handler implements RequestHandler<Map<String, String>, String> {
 
     @Override
     public String handleRequest(Map<String, String> event, Context context) {
-
+        log.info("### Event ### " + event.toString());
         return event.get("message").toUpperCase();
     }
 }
