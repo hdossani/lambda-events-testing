1ad7812500f64f88fee231725fac3fd862648517
hdossani <hiren.dossani@gmail.com>
2021-04-15 12:39:08 -0400

    updated for json error

--- src/main/java/com/lambda/basics/Handler.java
+++ src/main/java/com/lambda/basics/Handler.java
@@ -1,17 +1,22 @@
 package com.lambda.basics;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import lombok.extern.slf4j.Slf4j;
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.util.Map;
-@Slf4j
+
 public class Handler implements RequestHandler<Map<String, String>, String> {
 
     @Override
     public String handleRequest(Map<String, String> event, Context context) {
-        log.info("### Event ### " + event.toString());
-        return event.get("message").toUpperCase();
+        LambdaLogger logger = context.getLogger();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        // process event
+        logger.log("EVENT: " + gson.toJson(event));
+        logger.log("EVENT TYPE: " + event.getClass().toString());
+        return gson.toJson(event);
     }
 }
 
